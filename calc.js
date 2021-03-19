43e10a6161cf20751d2f4a8c2efeda0ebbd15904
Xeandrol <77493749+Xeandrol@users.noreply.github.com>
2021-03-19 18:50:36 +0100

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,25 @@
+function result(x, y, z, j) {
+  var x = document.getElementById("razioniNumber").value;
+  var y = document.getElementById("personeNumber").value;
+  var check = document.getElementById("bbqSalsa").checked;
+  var z = document.getElementById("cibiDiversi").value;
+  var j = 0;
+  if (x < y || x == 0 || y == 0) {
+    document.getElementById("bonus").innerHTML =
+      "Servono piu razioni Porcoddio";
+  } else {
+    if (check == true) {
+      j = 4;
+      document.getElementById("sad").innerHTML = ":)";
+    } else {
+      document.getElementById("sad").innerHTML = ":(";
+    }
+    document.getElementById("bonus").innerHTML = (x - y) * 3 + z * 2 + j;
+  }
+
+  /* else if ((check = false)) {
+    document.getElementById("sad").innerHTML = ":(";
+  } else if ((check = true)) {
+    document.getElementById("sad").innerHTML = ":)";
+  }*/
+}
